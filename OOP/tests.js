79fa4a8d26667007c5006873d5772c618590c274
Oluwatomi Akande <tomipaul95@gmail.com>
2017-03-16 01:28:21 +0100

    test cases

--- OOP/tests.js
+++ OOP/tests.js
@@ -3,10 +3,10 @@ const classes = require('./library.js');
 describe('hotel, room, single, doubleRoom, suite, guest and booking classes', function() {
 
   describe("create an hotel with a name", function() {
-    let Hotel = new classes.hotel('Premier');
+    let Hotel = new classes.Hotel('Premier');
     it("Hotel should be of type `object` and an instance of the hotel class", function() {
       expect(typeof Hotel).toEqual(typeof {});
-      expect(Hotel instanceof classes.hotel).toBeTruthy();
+      expect(Hotel instanceof classes.Hotel).toBeTruthy();
     });
 
     it("name, rooms and bookings should be a property of the hotel", function() {
@@ -21,8 +21,8 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
     });
 
     it("Hotel should addRoom and removeRoom", function() {
-      let Room1 = new classes.room(12);
-      let Room2 = new classes.room(13);
+      let Room1 = new classes.Room(12);
+      let Room2 = new classes.Room(13);
       Hotel.addRooms([Room1, Room2]);
       expect(Hotel.rooms).toEqual([Room1, Room2]);
       Hotel.removeRooms([Room1]);
@@ -30,9 +30,9 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
     });
 
     it("searchRoomByType should return all rooms with type `type`", function() {
-      let Single = new classes.single(1);
-      let Double = new classes.doubleRoom(2);
-      let Suite = new classes.suite(3)
+      let Single = new classes.Single(1);
+      let Double = new classes.DoubleRoom(2);
+      let Suite = new classes.Suite(3)
       Hotel.addRooms([Single, Double, Suite]);
       expect(Hotel.searchRoomByType('single')).toEqual([Single]);
       expect(Hotel.searchRoomByType('double')).toEqual([Double]);
@@ -49,10 +49,10 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
 
 
   describe("create a room with a roomNo", function() {
-    let Room = new classes.room(12);
+    let Room = new classes.Room(12);
     it("Room should be of type `object` and an instance of the room class", function() {
       expect(typeof Room).toEqual(typeof {});
-      expect(Room instanceof classes.room).toBeTruthy();
+      expect(Room instanceof classes.Room).toBeTruthy();
     });
 
     it("roomNo, roomType, guests should be properties of the room", function() {
@@ -73,11 +73,11 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
 
   describe("create a guest with a title and name", function() {
 
-    let Guest = new classes.guest('chief', 'Lakotun');
+    let Guest = new classes.Guest('chief', 'Lakotun');
 
     it("the Guest should be of type `object` and an instance of the guest class", function() {
       expect(typeof Guest).toEqual(typeof {});
-      expect(Guest instanceof classes.guest).toBeTruthy();
+      expect(Guest instanceof classes.Guest).toBeTruthy();
     });
 
     it('the guest should have properties `title` and `name` and method `cashOut`', function() {
@@ -95,13 +95,13 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
 
   describe("create a booking with guests, nights and room", function() {
 
-    let Guest = new classes.guest('chief', 'Lakotun');
-    let Single = new classes.single(10);
-    let Booking = new classes.booking([Guest], 3, Single);
+    let Guest = new classes.Guest('chief', 'Lakotun');
+    let Single = new classes.Single(10);
+    let Booking = new classes.Booking([Guest], 3, Single);
 
     it("the Booking should be of type `object` and an instance of the booking class", function() {
       expect(typeof Booking).toEqual(typeof {});
-      expect(Booking instanceof classes.booking).toBeTruthy();
+      expect(Booking instanceof classes.Booking).toBeTruthy();
     });
 
     it('the booking should have properties `guests`, `nights` and method `cashIn`', function() {
@@ -120,7 +120,7 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
     });
 
     it('Booking.cashIn should return `Guest"s balance is not sufficient for cost`', function() {
-      expect(Booking.cashIn(Guest)).toEqual("Guest's balance is not sufficient for cost");
+      expect(Booking.cashIn(Guest)).toEqual('Guest"s balance is not sufficient for cost');
       expect(Booking.cashedIn).toBe(false);
     });
 
@@ -139,12 +139,12 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
   describe("single, doubleRoom and suite classes", function() {
 
     describe("create a single with roomNo", function() {
-       let Single = new classes.single(10);
+       let Single = new classes.Single(10);
 
       it("Single should be of type `object` and an instance of the room and single class", function() {
         expect(typeof Single).toEqual(typeof {});
-        expect(Single instanceof classes.single).toBeTruthy();
-        expect(Single instanceof classes.room).toBeTruthy();
+        expect(Single instanceof classes.Single).toBeTruthy();
+        expect(Single instanceof classes.Room).toBeTruthy();
       });
 
       it("roomNo, roomType, guests should be properties of the room", function() {
@@ -162,12 +162,12 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
     });
 
     describe("create a doubleRoom with roomNo", function() {
-      let Double = new classes.doubleRoom(9);
+      let Double = new classes.DoubleRoom(9);
 
       it("Double should be of type `object` and an instance of the room and doubleRoom class", function() {
         expect(typeof Double).toEqual(typeof {});
-        expect(Double instanceof classes.doubleRoom).toBeTruthy();
-        expect(Double instanceof classes.room).toBeTruthy();
+        expect(Double instanceof classes.DoubleRoom).toBeTruthy();
+        expect(Double instanceof classes.Room).toBeTruthy();
       });;
 
       it("roomNo, roomType, guests should be properties of the room", function() {
@@ -185,12 +185,12 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
     });
 
     describe("create a suite with roomNo", function() {
-      let Suite = new classes.suite(8);
+      let Suite = new classes.Suite(8);
 
       it("Suite should be of type `object` and an instance of the room and doubleRoom class", function() {
         expect(typeof Suite).toEqual(typeof {});
-        expect(Suite instanceof classes.suite).toBeTruthy();
-        expect(Suite instanceof classes.room).toBeTruthy();
+        expect(Suite instanceof classes.Suite).toBeTruthy();
+        expect(Suite instanceof classes.Room).toBeTruthy();
       });;
 
       it("roomNo, roomType, guests should be properties of the room", function() {
@@ -211,14 +211,14 @@ describe('hotel, room, single, doubleRoom, suite, guest and booking classes', fu
   });
   
   describe('Guest makes a booking', function() {
-    let Guest1 = new classes.guest('Mr', 'Festus');
-    let Guest2 = new classes.guest('Mrs', 'Cinder');
-    let Single = new classes.single(1);
-    let Double = new classes.doubleRoom(2);
-    let Suite = new classes.suite(3);
+    let Guest1 = new classes.Guest('Mr', 'Festus');
+    let Guest2 = new classes.Guest('Mrs', 'Cinder');
+    let Single = new classes.Single(1);
+    let Double = new classes.DoubleRoom(2);
+    let Suite = new classes.Suite(3);
 
     it('create booking and process booking', function() {
-      let Booking = new classes.booking([Guest1,Guest2], 4, Double);
+      let Booking = new classes.Booking([Guest1,Guest2], 4, Double);
       expect(Booking.isValid()).toEqual(true);
       Guest1.cashOut(80000);
       expect(Guest1.balance).toEqual(80000);
